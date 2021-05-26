f7670d455b973a9c9480f2a0c83a018f33a56481
mineshmane <mineshmane94@gmail.com>
2021-05-26 23:02:58 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppointmentComponent } from './component/appointment/appointment.component';
+import { HomeComponent } from './component/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'apoint' , component:AppointmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentComponent } from './component/appointment/appointment.component';
+import { HomeComponent } from './component/home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatDividerModule, MatIconModule, MatInputModule, MatOptionModule, MatProgressBarModule, MatRadioModule, MatSelectModule, MatSliderModule } from '@angular/material';
+import {MatBadgeModule} from '@angular/material/badge';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,MatToolbarModule,MatBadgeModule, BrowserAnimationsModule,MatIconModule,
+    MatCardModule,FlexLayoutModule, MatInputModule,FormsModule,MatDividerModule,MatProgressBarModule,
+    MatSliderModule,MatOptionModule,MatSelectModule,MatRadioModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
